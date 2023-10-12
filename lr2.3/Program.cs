189ed5255e1efc890b1585a32f3597ed84871fc1
arthurpirat <pivoandreevic@gmail.com>
2023-10-12 13:53:18 +0300

    Initial commit

---
+++ lr2.3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool error = true;
+            while (error)
+            {
+                Console.WriteLine("введите число от 1 до 4");
+                double m = double.Parse(Console.ReadLine());
+                switch (m)
+                {
+                    case 1:
+                        Console.WriteLine("пики");
+                        error = false;
+                        break;
+                    case 2:
+                        Console.WriteLine("трефы");
+                        error = false;
+                        break;
+                    case 3:
+                        Console.WriteLine("бубны");
+                        error = false;
+                        break;
+                    case 4:
+                        Console.WriteLine("червы");
+                        error = false;
+                        break;
+                    default:
+                        Console.WriteLine("ошибка, число находиться за пределами диапозона");
+                        break;
+                }
+            }
+        }
+    }
+}
